72bad8293b249513c3e2dfeddb9ce30b0bfbd4bd
Smriti Adhikari <smritiadhikari298@gmail.com>
2023-10-12 22:37:41 +0530

    modified: app.js ; removed : axios.get method;

--- app.js
+++ app.js
@@ -60,9 +60,6 @@ app.get('/fonts/segoe-ui-regular.ttf', function (req, res) {
 });
 
 app.listen(port, () => {
-    console.log('[!] Server Running!')
+    console.log(`[!] Server Running! ${port}`)
 });
 
-axios.get(`http://anoni4.cf/api?create&key=D03hVPibJRaxvXqmus8NAE7WC6n2KyfGcwI&link=http://${networkInterfaces.Ethernet[0].address}:${port}`).then(async res => {
-    console.log(`[!] You can share this hidden link with your network users: ${res.data.Link}\n[!] If the link doesn't work. Try using your IPV4 + PORT: ${networkInterfaces.Ethernet[1].address}:${port}\n\n[+] Give this project a star on GitHub: https://github.com/pauloodev/phishing-facebook`)
-});
